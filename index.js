0a9e0e7ab7c88304189157b3d706a60d2b79556f
Metwet <tselm.matv@gmail.com>
2023-07-30 15:27:43 +0300

    timer done

--- index.js
+++ index.js
@@ -5,7 +5,37 @@ const timerEl = document.querySelector('span');
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+
+  let interval;
+
+  return (seconds) => {
+
+    if(seconds > 359999){
+      seconds = 359999;
+    }
+    
+    const formatTime = (time) => {
+      return String(time).padStart(2, '0');
+    };
+
+    if(interval) {
+      clearInterval(interval);
+    }
+
+    interval = setInterval(() => {
+      if (seconds <= 0) {
+        clearInterval(interval);
+        timerEl.textContent = '00:00:00';
+      } else {
+        const hours = Math.floor(seconds / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+        const remainingSeconds = seconds % 60;
+        const formattedTime = `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(remainingSeconds)}`;
+        timerEl.textContent = formattedTime;
+        seconds--;
+      }
+    }, 1000);
+  };
 };
 
 const animateTimer = createTimerAnimator();
@@ -14,16 +44,14 @@ inputEl.addEventListener('input', () => {
   // Очистите input так, чтобы в значении
   // оставались только числа
   const formatInput = (value) => {
-    return value.replace(/\D/g, '').replace(/(\d{2})(?=\d)/g, '$1:');
+    return value.replace(/\D/g, '');
   };
 
-  inputEl.value = formatInput(inputEl.value).slice(0, 8);
+  inputEl.value = formatInput(inputEl.value);
 });
 
 buttonEl.addEventListener('click', () => {
   const seconds = Number(inputEl.value);
-
-  console.log(seconds);
 
   animateTimer(seconds);
 
